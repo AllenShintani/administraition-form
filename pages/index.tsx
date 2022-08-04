e073141c4cf732a95f119f84a620486f1f493eb5
konjikun <s1f102200378@iniad.org>
2022-08-05 01:39:31 +0900

    iframeの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,49 +10,9 @@ const Container = styled.div`
   height: 100vh;
   min-height: 100vh;
   padding: 0 0.5rem;
+
 `
 
-const Main = styled.main`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 5rem 0;
-`
-
-const Title = styled.h1`
-  margin: 0;
-  font-size: 4rem;
-  line-height: 1.15;
-  text-align: center;
-
-  a {
-    color: #0070f3;
-    text-decoration: none;
-  }
-
-  a:hover,
-  a:focus,
-  a:active {
-    text-decoration: underline;
-  }
-`
-
-const Description = styled.p`
-  font-size: 1.5rem;
-  line-height: 1.5;
-  text-align: center;
-`
-
-const Code = styled.code<{ bgColor: string }>`
-  padding: 0.75rem;
-  font-family: Menlo, 'Monaco, Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono',
-    'Bitstream Vera Sans Mono', 'Courier New', monospace;
-  font-size: 1.1rem;
-  background: ${(props) => props.bgColor};
-  border-radius: 5px;
-`
 
 const Grid = styled.div`
   display: flex;
@@ -66,18 +26,6 @@ const Grid = styled.div`
     flex-direction: column;
     width: 100%;
   }
-`
-
-const Card = styled.a`
-  width: 45%;
-  padding: 1.5rem;
-  margin: 1rem;
-  color: inherit;
-  text-align: left;
-  text-decoration: none;
-  border: 1px solid #eaeaea;
-  border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
 
   :hover,
   :focus,
@@ -98,28 +46,16 @@ const Card = styled.a`
   }
 `
 
-const Footer = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100px;
-  border-top: 1px solid #eaeaea;
-
-  a {
-    display: flex;
-    flex-grow: 1;
-    align-items: center;
-    justify-content: center;
-  }
-`
-
-const Logo = styled.span`
-  height: 1em;
-  margin-left: 0.5rem;
-`
 
 const Home: NextPage = () => {
+  function pm(){
+    return console.log(0)
+  }
+
+  function cancel(){
+    return console.log(1)
+  }
+
   return (
     <Container>
       <Head>
@@ -128,50 +64,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="favicon.ico" />
       </Head>
 
-      <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
 
-        <Description>
-          Get started by editing <Code bgColor="#fafafa">pages/index.js</Code>
-        </Description>
 
         <Grid>
-          <Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
+  <h3>水道局に
+  <ul>
+    <li>家族の関係</li>
+    <li>氏名</li>
+    <li>メールアドレス</li>
+    <li>電話番号</li>
+    </ul>
+    の情報の取得を許可しますか？
+    </h3>
+   </Grid>
 
-          <Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-
-          <Card href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </Card>
-
-          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </Card>
-        </Grid>
-      </Main>
-
-      <Footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Logo>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </Logo>
-        </a>
-      </Footer>
+      <Grid><button onClick={pm} >
+      <strong>ok</strong>
+    </button>
+    
+    <button onClick={cancel}>
+      <strong>no</strong>
+    </button></Grid>
     </Container>
   )
 }
